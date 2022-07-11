5e61ee3320c26554b68363e756b1aa64b29807e2
hwangchangho <changho3755@gmail.com>
2022-07-11 20:46:41 +0900

    /usr/article/write 라고 입력하면 게시물 제목, 내용 입력받기

--- src/com_hch_exam_make/Main.java
+++ src/com_hch_exam_make/Main.java
@@ -16,7 +16,20 @@ public class Main {
       if (cmd.equals("exit")){
         break;
       }
-      System.out.println("입력된 명령어 : " + cmd);
+      else if(cmd.equals("/usr/article/write")){
+        System.out.println(" - 게시물 등록 - ");
+        System.out.printf("제목 : ");
+        String title = sc.next();
+        System.out.printf("내용 : ");
+        String body = sc.next();
+
+        int num = 1;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", num);
+      }
+      else{
+        System.out.println("입력된 명령어 : " + cmd);
+      }
     }
     System.out.println("== 프로그램 종료 ==");
   }

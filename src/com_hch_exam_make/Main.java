d8a52c9ffcd817593555442d61ab3d74a41be42f
hwangchangho <changho3755@gmail.com>
2022-07-22 18:03:28 +0900

    97강, Util.reverseList 를 이용해서 정렬로직 간소화

--- src/com_hch_exam_make/Main.java
+++ src/com_hch_exam_make/Main.java
@@ -16,24 +16,24 @@ public class Main {
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
-    int articlelastNum = 0;
+    int articleLastNum = 0;
 
-    List<Article> article = new ArrayList<>();
+    List<Article> articles = new ArrayList<>();
 
-    makeTestData(article);
+    makeTestData(articles);
 
-    if ( article.size() > 0) {
-      articlelastNum = article.get(article.size() - 1).num;
+    if ( articles.size() > 0) {
+      articleLastNum = articles.get(articles.size() - 1).num;
     }
 
     while(true){
-      System.out.printf("명령)");
+      System.out.print("명령)");
       String cmd = sc.next();
 
       Rq rq= new Rq(cmd);
       Map<String, String> params = rq.getParams();
 
-      if (rq.getUrlPath().equals("exit")){
+      if (cmd.equals("exit")){
         break;
       }
       else if (rq.getUrlPath().equals("/usr/article/list")){ //cmd를 rq.getUrlPath()로바꾸면 아무리 복잡한 명령어를 넣어도 ? 이전으로 호출
@@ -42,26 +42,24 @@ public class Main {
         System.out.println("번호 / 제목 / 내용");
         System.out.println("-----------------");
 
+        List<Article> sortedArticle = articles;
+
         boolean orderByIdDesc = true;
         if(params.get("orderBy") != null && params.get("orderBy").equals("IdAsc")){
           orderByIdDesc = false;
         }
         if(orderByIdDesc){
-          for(int i = article.size() - 1; i >= 0; i--){
-            Article articles = article.get(i);
-            System.out.printf("%d / %s / %s\n", articles.num, articles.title, articles.body);
-          }
+          sortedArticle = Util.reverseList(sortedArticle);
         }
-        else{
-          for(Article articles : article){
-            System.out.printf("%d / %s / %s\n", articles.num, articles.title, articles.body);
+
+          for(Article article : sortedArticle){
+            System.out.printf("%d / %s / %s\n", article.num, article.title, article.body);
           }
-        }
 
       }
       else if (rq.getUrlPath().equals("/usr/article/detail")){
 
-        if(params.containsKey("num") == false){
+        if(!params.containsKey("num")){
           System.out.println("번호를 입력해주세요");
           continue;
         }
@@ -78,39 +76,39 @@ public class Main {
 
 
 
-        if(article.isEmpty()){//article이 비어있냐?? 라고물어보는함수// ==  article.size() == 0
+        if(articles.isEmpty()){//article이 비어있냐?? 라고물어보는함수// ==  article.size() == 0
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        Article articles = article.get(num-1);
+        Article article = articles.get(num-1);
 
-        if(num > article.size()){
+        if(num > articles.size()){
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
         System.out.println(" - 게시물 상세보기 - ");
-        System.out.printf("번호 : %d\n", articles.num);
-        System.out.printf("제목 : \"%s\"\n", articles.title);
-        System.out.printf("내용 : \"%s\"\n", articles.body);
+        System.out.printf("번호 : %d\n", article.num);
+        System.out.printf("제목 : \"%s\"\n", article.title);
+        System.out.printf("내용 : \"%s\"\n", article.body);
 
       }
       else if(rq.getUrlPath().equals("/usr/article/write")){
         System.out.println(" - 게시물 등록 - ");
-        System.out.printf("제목 : ");
+        System.out.print("제목 : ");
         String title = sc.next();
-        System.out.printf("내용 : ");
+        System.out.print("내용 : ");
         String body = sc.next();
 
-        int num = ++articlelastNum; // == articleLastNum + 1; articleLastNum++;
+        int num = ++articleLastNum; // == articleLastNum + 1; articleLastNum++;
 
-        Article articles = new Article(num, title, body);
+        Article article = new Article(num, title, body);
 
-        article.add(articles);
+        articles.add(article);
 
-        System.out.println("입력받은 객체 : "+ articles);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", articles.num);
+        System.out.println("입력받은 객체 : "+ article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.num);
       }
       else{
         System.out.println("입력된 명령어 : " + cmd);
@@ -184,5 +182,15 @@ class Util {
   }
   static String getUrlPathFromUrl(String url) {
     return url.split("\\?", 2)[0];
+  }
+
+  // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다. 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
+  public static<T> List<T> reverseList(List<T> list) {
+    List<T> reverse = new ArrayList<>(list.size());
+
+    for ( int i = list.size() - 1; i >= 0; i-- ) {
+      reverse.add(list.get(i));
+    }
+    return reverse;
   }
 }

0353ec5a0e8d59246010692efe0f6283bd7c3830
hwangchangho <changho3755@gmail.com>
2022-07-27 14:51:58 +0900

    112강, UsrArticleController 클래스 도입, 구현

--- src/com_hch_exam_make/App.java
+++ src/com_hch_exam_make/App.java
@@ -1,26 +1,9 @@
 package com_hch_exam_make;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class App {
-  int articleLastNum;
-
-  List<Article> articles;
-
-  App(){
-    articleLastNum = 0;
-    articles = new ArrayList<>();
-  }
-
-  void makeTestData(){
-    for( int i = 0; i < 100; i++){
-      int id = i + 1;
-      articles.add(new Article(id, "제목" + id, "내용" + id));
-    }
-  }
 
   void run(){
     Scanner sc = Container.sc;
@@ -28,11 +11,6 @@ public class App {
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
-    makeTestData();
-
-    if ( articles.size() > 0) {
-      articleLastNum = articles.get(articles.size() - 1).num;
-    }
 
     while(true){
       System.out.print("명령)");
@@ -46,19 +24,19 @@ public class App {
         break;
       }
       else if (rq.getUrlPath().equals("/usr/article/list")){ //cmd를 rq.getUrlPath()로바꾸면 아무리 복잡한 명령어를 넣어도 ? 이전으로 호출
-        actionUsrArticleList(rq);
+        Container.usrArticleController.actionList(rq); // container로가서 usrarticlecontroller가실행되면서actionlist로들어간ㄷ?
       }
       else if (rq.getUrlPath().equals("/usr/article/detail")){
-        actionUsrArticleDetail(rq);
+        Container.usrArticleController.actionDetail(rq);
       }
       else if(rq.getUrlPath().equals("/usr/article/write")){
-        actionUsrArticleWrite(rq, sc);
+        Container.usrArticleController.actionWrite(rq, sc);
       }
       else if(rq.getUrlPath().equals("/usr/article/modify")){
-        actionUsrArticleModify(rq, sc);
+        Container.usrArticleController.actionModify(rq, sc);
       }
       else if(rq.getUrlPath().equals("/usr/article/delete")){
-        actionUsrArticleDelete(rq);
+        Container.usrArticleController.actionDelete(rq);
       }
       else{
         System.out.println("입력된 명령어 : " + cmd);
@@ -67,202 +45,6 @@ public class App {
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
-
-  }
-  private void actionUsrArticleDelete(Rq rq) {
-    Map<String, String> params = rq.getParams();
-
-    if(!params.containsKey("num")){
-      System.out.println("번호를 입력해주세요");
-      return;
-    }
-
-    int num = 0;
-
-    try{
-      num = Integer.parseInt(params.get("num")); // num이 들어있나 확인/ 들어있는 String값num을 int값으로 변환
-    }
-    catch(NumberFormatException e){
-      System.out.println("번호를 정수형 형태로 입력해주세요");
-      return; //continue 대신 쓸수 있는 함수는 return 이다. continue가 밑에껄 생략하고 위에올라가는것처럼 return도 똑같다.
-      //continue는 반복문 안에서만 효율을 발휘한다.
-    }
-
-
-
-    if(articles.isEmpty()){//article이 비어있냐?? 라고물어보는함수// ==  article.size() == 0
-      System.out.println("게시물이 존재하지 않습니다.");
-      return;
-    }
-
-    Article article = articles.get(num-1);
-
-    if(num > articles.size()){
-      System.out.println("게시물이 존재하지 않습니다.");
-      return;
-    }
-
-    articles.remove(article);
-    System.out.println(article.num + "번 게시물이 삭제되었습니다.");
-
-
-  }
-
-  private void actionUsrArticleModify(Rq rq, Scanner sc) {
-
-    Map<String, String> params = rq.getParams();
-
-    if(!params.containsKey("num")){
-      System.out.println("번호를 입력해주세요");
-      return;
-    }
-
-    int num = 0;
-
-    try{
-      num = Integer.parseInt(params.get("num")); // num이 들어있나 확인/ 들어있는 String값num을 int값으로 변환
-    }
-    catch(NumberFormatException e){
-      System.out.println("번호를 정수형 형태로 입력해주세요");
-      return; //continue 대신 쓸수 있는 함수는 return 이다. continue가 밑에껄 생략하고 위에올라가는것처럼 return도 똑같다.
-      //continue는 반복문 안에서만 효율을 발휘한다.
-    }
-
-
-
-    if(articles.isEmpty()){//article이 비어있냐?? 라고물어보는함수// ==  article.size() == 0
-      System.out.println("게시물이 존재하지 않습니다.");
-      return;
-    }
-
-    Article article = articles.get(num-1);
-
-    if(num > articles.size()){
-      System.out.println("게시물이 존재하지 않습니다.");
-      return;
-    }
-
-    System.out.print("새 제목 : ");
-    article.title = sc.next();
-    System.out.print("새 내용 : ");
-    article.body = sc.next();
-
-    System.out.printf("%d번 게시물이 수정되었습니다.\n", num);
-  }
-
-  private void actionUsrArticleWrite(Rq rq, Scanner sc) {
-    System.out.println(" - 게시물 등록 - ");
-    System.out.print("제목 : ");
-    String title = sc.next();
-    System.out.print("내용 : ");
-    String body = sc.next();
-
-    int num = ++articleLastNum; // == articleLastNum + 1; articleLastNum++;
-
-    Article article = new Article(num, title, body);
-
-    articles.add(article);
-
-    System.out.println("입력받은 객체 : "+ article);
-    System.out.printf("%d번 게시물이 등록되었습니다.\n", article.num);
-  }
-
-  private void actionUsrArticleDetail(Rq rq) {
-    Map<String, String> params = rq.getParams();
-
-    if(!params.containsKey("num")){
-      System.out.println("번호를 입력해주세요");
-      return;
-    }
-
-    int num = 0;
-
-    try{
-      num = Integer.parseInt(params.get("num")); // num이 들어있나 확인/ 들어있는 String값num을 int값으로 변환
-
-      if(num > articles.size()){
-        System.out.println("게시물이 존재하지 않습니다.");
-        return;
-      }
-      //길이가 벗어나게 하면 오류가 떠서 아래에 try catch로 다시 만들어줬는데 그밑을보니 이미 오류가 났을경우 처리되는 함수가 만들어져있음
-      //그래서 이거를 안으로 넣어줌 따로 trycatch를 만들어줄 필요가 없음
-    }
-    catch(NumberFormatException e){
-      System.out.println("번호를 정수형 형태로 입력해주세요");
-      return; //continue 대신 쓸수 있는 함수는 return 이다. continue가 밑에껄 생략하고 위에올라가는것처럼 return도 똑같다.
-      //continue는 반복문 안에서만 효율을 발휘한다.
-    }
-
-
-
-    if(articles.isEmpty()){//article이 비어있냐?? 라고물어보는함수// ==  article.size() == 0
-      System.out.println("게시물이 존재하지 않습니다.");
-      return;
-    }
-
-    Article article = articles.get(num-1);
-
-//    try{
-//      Article article = articles.get(num-1);
-//
-//      System.out.println(" - 게시물 상세보기 - ");
-//      System.out.printf("번호 : %d\n", article.num);
-//      System.out.printf("제목 : \"%s\"\n", article.title);
-//      System.out.printf("내용 : \"%s\"\n", article.body);
-//    }
-//    catch(IndexOutOfBoundsException e){
-//      System.out.println("게시물 범위를 벗어났습니다.");
-//      return;
-//    }
-
-
-    System.out.println(" - 게시물 상세보기 - ");
-    System.out.printf("번호 : %d\n", article.num);
-    System.out.printf("제목 : \"%s\"\n", article.title);
-    System.out.printf("내용 : \"%s\"\n", article.body);
-  }
-
-  private void actionUsrArticleList(Rq rq) {
-    Map<String, String> params = rq.getParams();
-
-    List<Article> filteredArticle = articles;
-
-    String searchKeyword = params.get("searchKeyword");
-
-    if(searchKeyword != null){
-      searchKeyword = params.get("searchKeyword");
-
-      filteredArticle = new ArrayList<>();
-
-      for(Article article : articles){
-        boolean matched = article.title.contains(searchKeyword) || article.body.contains(searchKeyword);
-
-        if(matched) {
-          filteredArticle.add(article);
-        }
-      }
-    }
-
-//    System.out.println(filteredArticle);
-
-    System.out.println(" - 게시물 리스트 - ");
-    System.out.println("-----------------");
-    System.out.println("번호 / 제목 / 내용");
-    System.out.println("-----------------");
-
-    List<Article> sortedArticle = filteredArticle;
-
-    boolean orderByIdDesc = true;
-    if(params.containsKey("orderBy") && params.get("orderBy").equals("IdAsc")){
-      orderByIdDesc = false;
-    }
-    if(orderByIdDesc){
-      sortedArticle = Util.reverseList(sortedArticle);
-    }
-
-    for(Article article : sortedArticle){
-      System.out.printf("%d / %s / %s\n", article.num, article.title, article.body);
-    }
 
   }
 
--- src/com_hch_exam_make/Container.java
+++ src/com_hch_exam_make/Container.java
@@ -3,9 +3,14 @@ package com_hch_exam_make;
 import java.util.Scanner;
 
 public class Container {// container 는 공공재자원으로 사용할 것들을 모아두는것이고 한개만 존재할 수 있따.
-  static Scanner sc; // main이 static 이기 때문에 static을 붙혀줘야한다.
+  static Scanner sc;
 
-  static{
+  static UsrArticleController usrArticleController;
+
+  static{ //static은 프로그램이 실행된뒤에 딱한번 실행된다? 그래서 controller생성자는 여기에 객체가 생길때 실행이되나?
     sc = new Scanner(System.in);
+
+    usrArticleController = new UsrArticleController();
   }
 }
+//이 컨테이너는 app이 실행되기전 먼저 실행이되나?
